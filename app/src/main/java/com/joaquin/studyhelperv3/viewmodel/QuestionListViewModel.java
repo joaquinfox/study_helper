7493ce4f82d89fd3982b1dca2a6e76da85a56154
Jimmy <joaquin.fox@gmail.com>
2022-08-13 14:57:51 -0700

    viewmodels

--- app/src/main/java/com/joaquin/studyhelperv3/viewmodel/QuestionListViewModel.java
+++ app/src/main/java/com/joaquin/studyhelperv3/viewmodel/QuestionListViewModel.java
@@ -1,4 +1,20 @@
 package com.joaquin.studyhelperv3.viewmodel;
 
+
+import android.app.Application;
+import com.joaquin.studyhelperv3.model.Question;
+import com.joaquin.studyhelperv3.repo.StudyRepository;
+import java.util.List;
+
 public class QuestionListViewModel {
+
+    private StudyRepository studyRepo;
+
+    public QuestionListViewModel(Application application) {
+        studyRepo = StudyRepository.getInstance(application.getApplicationContext());
+    }
+
+    public List<Question> getQuestions(long subjectId) {
+        return studyRepo.getQuestions(subjectId);
+    }
 }
--- app/src/main/java/com/joaquin/studyhelperv3/viewmodel/SubjectListViewModel.java
+++ app/src/main/java/com/joaquin/studyhelperv3/viewmodel/SubjectListViewModel.java
@@ -1,4 +1,24 @@
 package com.joaquin.studyhelperv3.viewmodel;
 
+
+import android.app.Application;
+import com.joaquin.studyhelperv3.model.Subject;
+import com.joaquin.studyhelperv3.repo.StudyRepository;
+import java.util.List;
+
 public class SubjectListViewModel {
+
+    private StudyRepository studyRepo;
+
+    public SubjectListViewModel(Application application) {
+        studyRepo = StudyRepository.getInstance(application.getApplicationContext());
+    }
+
+    public List<Subject> getSubjects() {
+        return studyRepo.getSubjects();
+    }
+
+    public void addSubject(Subject subject) {
+        studyRepo.addSubject(subject);
+    }
 }
