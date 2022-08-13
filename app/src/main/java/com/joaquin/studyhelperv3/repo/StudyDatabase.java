9fac2ccb8cd6841fbe2d130ac4703e72ceff7162
Jimmy <joaquin.fox@gmail.com>
2022-08-13 15:51:38 -0700

    database

--- app/src/main/java/com/joaquin/studyhelperv3/repo/StudyDatabase.java
+++ app/src/main/java/com/joaquin/studyhelperv3/repo/StudyDatabase.java
@@ -1,4 +1,13 @@
 package com.joaquin.studyhelperv3.repo;
 
-public class StudyDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import com.joaquin.studyhelperv3.model.Question;
+import com.joaquin.studyhelperv3.model.Subject;
+
+@Database(entities = {Question.class, Subject.class}, version = 1)
+public abstract class StudyDatabase extends RoomDatabase {
+
+    public abstract QuestionDao questionDao();
+    public abstract SubjectDao subjectDao();
 }
